649deda3eb38a271cdf45eefa2338885794a0707
usemanusai <use.manus.ai@gmail.com>
2025-08-05 17:46:58 +0200

    Upload src/web/jaegis_cockpit/frontend/vite.config.js

---
+++ src/web/jaegis_cockpit/frontend/vite.config.js
@@ -0,0 +1,14 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	server: {
+		port: 5173,
+		host: true
+	},
+	preview: {
+		port: 4173,
+		host: true
+	}
+});
