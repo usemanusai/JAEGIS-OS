077bf942cc28a3f14d7046295191c98bfe2330b5
usemanusai <use.manus.ai@gmail.com>
2025-08-05 18:40:56 +0200

    Upload src/specialized-systems/JAEGIS/JAEGIS-METHOD/build-web-agent.cfg.js

---
+++ src/specialized-systems/JAEGIS/JAEGIS-METHOD/build-web-agent.cfg.js
@@ -0,0 +1,9 @@
+// build-web-agent.cfg.js
+// This file contains the configuration for the build-web-agent.js script.
+
+module.exports = {
+  orchestrator_agent_prompt: "./jaegis-agent/web-jaegis-orchestrator-agent.md",
+  agent_cfg: "./jaegis-agent/web-jaegis-orchestrator-agent.cfg.md",
+  asset_root: "./jaegis-agent/",
+  build_dir: "./build/",
+};
